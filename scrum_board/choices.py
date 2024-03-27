926065624ddd34c8e3a162b585b79cda19f43901
Zwogli <Mathiaskohler@web.de>
2024-03-27 11:00:37 +0100

    Edit task model

---
+++ scrum_board/choices.py
@@ -0,0 +1,22 @@
+PRIORITY_CHOICES = (
+    ('high', 'High'),
+    ('mid', 'Mid'),
+    ('low', 'Low'),
+)
+
+COLOR_CHOICES = (
+    ('#FF0000', 'Red'),
+    ('#00FF00', 'Green'),
+    ('#0000FF', 'Blue'),
+    ('#FFFF00', 'Yellow'),
+    ('#FF00FF', 'Magenta'),
+    ('#00FFFF', 'Cyan'),
+    # Weitere Farben nach Bedarf hinzufügen
+)
+
+BOARD_COLUMN_CHOICES = (
+    ('board-column-todo', 'To Do'),
+    ('board-column-progress', 'In Progress'),
+    ('board-column-feedback', 'Feedback'),
+    ('board-column-done', 'Done')
+)
--- scrum_board/models.py
+++ scrum_board/models.py
@@ -4,51 +4,35 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+from . import choices
+
+def is_due_date_into_future(due_date):
+    return due_date < datetime.date.today()
+
 # Create your models here.
 class Task(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description= models.TextField(max_length=250)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
+    
     created_at = models.DateField(default=datetime.date.today)
     due_date = models.DateField()
     
-    PRIORITY_CHOICES = (
-        ('high', 'high'),
-        ('mid', 'mid'),
-        ('low', 'low'),
-    )
-    priority = models.CharField(max_length=7, choices=PRIORITY_CHOICES, default='low')
-    
-    COLOR_CHOICES = (
-        ('#FF0000', 'Red'),
-        ('#00FF00', 'Green'),
-        ('#0000FF', 'Blue'),
-        ('#FFFF00', 'Yellow'),
-        ('#FF00FF', 'Magenta'),
-        ('#00FFFF', 'Cyan'),
-        # Weitere Farben nach Bedarf hinzufügen
-    )
-    color = models.CharField(max_length=7, choices=COLOR_CHOICES, default='#FF0000')
-    
-    BOARD_COLUMN_CHOICES = (
-        ('board-column-todo', 'board-column-todo'),
-        ('board-column-progress', 'board-column-progress'),
-        ('board-column-feedback', 'board-column-feedback'),
-        ('board-column-done', 'board-column-done')
-    )
-    board_column = models.CharField(max_length=21, choices=BOARD_COLUMN_CHOICES, default='board-column-todo')
-    
-    
-    def task_id(self):
-        return self.id
+    priority = models.CharField(max_length=7, choices=choices.PRIORITY_CHOICES, default='low')
+    color = models.CharField(max_length=7, choices=choices.COLOR_CHOICES, default='#FF0000')
+    board_column = models.CharField(max_length=21, choices=choices.BOARD_COLUMN_CHOICES, default='board-column-todo')
     
     def clean(self):
-        if self.due_date < datetime.date.today():
+        if is_due_date_into_future(self.due_date):
             raise ValidationError(_('Due date cannot be in the past.'))
 
     def save(self, *args, **kwargs):
         self.full_clean()  # run clean() method before saving
         super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return f'({self.id}) Title: {self.title}, Priority: {self.priority}, Board-column: {self.board_column}'
