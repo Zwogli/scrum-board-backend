a0e0559597ca9319ea1b5875fd11aaea55add511
Zwogli <Mathiaskohler@web.de>
2024-03-27 10:24:52 +0100

    Create task model

--- scrum_board/models.py
+++ scrum_board/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import datetime
+from django.conf import settings
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description= models.TextField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateField(default=datetime.date.today)
+    due_date = models.DateField()
+    PRIORITY_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+    )
+    
+    def task_id(self):
+        return self.id
