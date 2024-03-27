22d7d591d0aea643243210f88dec27281e44c76e
Zwogli <Mathiaskohler@web.de>
2024-03-27 11:10:42 +0100

    Test view task

--- scrum_board/views.py
+++ scrum_board/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .models import Task
+from .serializers import TaskSerializer
 
 
 # Create your views here.
 class TaskView(APIView):
-    pass
+    def get(self, request, format=None):
+        tasks = Task.objects
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
