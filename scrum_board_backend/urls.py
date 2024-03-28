f2942cef033e84e59af9cc3242708967e5061834
Zwogli <Mathiaskohler@web.de>
2024-03-28 09:30:34 +0100

    Add redirect empty url

--- scrum_board_backend/urls.py
+++ scrum_board_backend/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+
 
 from scrum_board.views import TaskView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/tasks/')),
     path('admin/', admin.site.urls),
     path('tasks/', TaskView.as_view()),
 ]
