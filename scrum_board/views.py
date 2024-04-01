077afeff76ebcf179865755df1006b326788180b
Zwogli <Mathiaskohler@web.de>
2024-04-01 10:36:41 +0200

    Add authention

--- scrum_board/views.py
+++ scrum_board/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -9,6 +11,9 @@ from .serializers import TaskSerializer
 
 # Create your views here.
 class TaskView(APIView):
+    authentication_classes = [TokenAuthentication]  # Token must available
+    permission_classes = [IsAuthenticated]          # User must loged in
+    
     def get(self, request, format=None):
         tasks = Task.objects
         serializer = TaskSerializer(tasks, many=True)
