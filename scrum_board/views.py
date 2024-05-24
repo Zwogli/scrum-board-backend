1e2ad19afb2a8eae81402fe87c80df5082a026fa
Zwogli <Mathiaskohler@web.de>
2024-05-24 09:24:54 +0200

    Add post task view

--- scrum_board/views.py
+++ scrum_board/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
@@ -18,3 +19,10 @@ class TaskView(APIView):
         tasks = Task.objects
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
