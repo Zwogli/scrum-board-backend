4c717265415877ab4ae9c170a3e60e710bd24e8a
Zwogli <Mathiaskohler@web.de>
2024-06-05 11:07:18 +0200

    Rework websocket

--- scrum_board/consumers.py
+++ scrum_board/consumers.py
@@ -1,18 +1,38 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class TaskConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class TaskConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = 'tasks'
 
-    def disconnect(self, close_code):
-        pass
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    def receive(self, text_data):
-        pass
-    
-    def inform_clients(self, event):
-        # Send a message to the WebSocket
-        self.send(text_data=json.dumps({
-            'message': event['message']
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        task = data['task']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'task_update',
+                'task': task
+            }
+        )
+
+    async def task_update(self, event):
+        task = event['task']
+
+        await self.send(text_data=json.dumps({
+            'task': task
         }))
