74bd70fd1100cecbafb8905513f3f5f14526f6bd
Zwogli <Mathiaskohler@web.de>
2024-06-12 08:51:47 +0200

    Fix test_logout

--- userAuth/tests.py
+++ userAuth/tests.py
@@ -4,5 +4,5 @@ from common_tests.base_tests import BaseTestSetup
 # Create your tests here.
 class LogoutTest(BaseTestSetup):
     def test_logout(self):
-        response =self.client.get('/logout/')
+        response =self.client.post('/logout/')
         self.assertEqual(response.status_code, 200)
