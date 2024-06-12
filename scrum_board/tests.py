652dfbb3afb92555768ce605c41a263c8660449a
Zwogli <Mathiaskohler@web.de>
2024-06-12 08:18:36 +0200

    Init test scrum board

--- scrum_board/tests.py
+++ scrum_board/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class ScrumBoardTest(TestCase):
+    def test_scrumboardpage(self):
+        response =self.client.get('/tasks/')
+        self.assertEqual(response.satus_code, 200)
