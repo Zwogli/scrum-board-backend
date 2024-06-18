a48e92811132c4ee9ba8eb83c953ff86815623c8
Zwogli <Mathiaskohler@web.de>
2024-06-18 08:49:27 +0200

    Refactor tests

--- common_tests/base_tests.py
+++ common_tests/base_tests.py
@@ -3,30 +3,46 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
+from scrum_board.models import Task
 from datetime import date, timedelta
 
 class BaseTestSetup(TestCase):
+    TEST_TASK_DATA = {
+        'id': 1,
+        'title': 'Test',
+        'description': 'Test description',
+        'author': 'test_user',
+        'author_username': 'test_user',
+        'created_at': '2024-06-12',
+        'due_date': (date.today() + timedelta(days=1)).strftime('%Y-%m-%d'),
+        'priority': 'high',
+        'color': 'red',
+        'board_column': 'board-column-todo'
+    }
+
+
+    UPDATED_TASK_DATA = {
+        'title': 'Updated Task',
+        'description': 'Updated description',
+        'priority': 'low',
+        'color': 'blue',
+        'board_column': 'board-column-done'
+    }
+        
+        
     def setUp(self):
         self.client = APIClient()
         self.user = User.objects.create_user(username='test_user', password='test_password')
         self.token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         
-        # Set future date for created_at and due_date
-        future_date = (date.today() + timedelta(days=10)).isoformat()
+        
+    def create_task(self):
+        task_data = self.TEST_TASK_DATA.copy()
+        task_data['author'] = self.user
+        task = Task.objects.create(**task_data)
+        return task
         
         
-        self.TEST_TASK_DATA = (
-            ('id', 1),
-            ('title', 'Test'),
-            ('description', 'Test description'),
-            ('author', 'test_user'),
-            ('author_username', 'test_user'),
-            ('created_at', future_date),
-            ('due_date', future_date),
-            ('priority', 'high'),
-            ('color', 'red'),
-            ('board_column', 'board-column-todo')
-        )
     
     
--- scrum_board/tests.py
+++ scrum_board/tests.py
@@ -6,14 +6,12 @@ from rest_framework import status
 # Create your tests here.
 class ScrumBoardTest(BaseTestSetup):
     def test_get_scrumBoard(self):
-        # self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         response =self.client.get('/tasks/')
         self.assertEqual(response.status_code, 200)
     
     
     def test_post_scrumboard(self):
-        task_data = dict(self.TEST_TASK_DATA)
-        task_data.pop('author', None)
+        task_data = self.TEST_TASK_DATA.copy()
         task_data['author_username'] = self.user.username
         response =self.client.post('/tasks/', data=task_data, format='json')
         self.assertEqual(response.status_code, 201)
@@ -22,7 +20,6 @@ class ScrumBoardTest(BaseTestSetup):
         self.assertEqual(response.data['id'], task_data['id'])
         self.assertEqual(response.data['title'], task_data['title'])
         self.assertEqual(response.data['description'], task_data['description'])
-        # self.assertEqual(response.data['author'], task_data['author'])
         self.assertEqual(response.data['author_username'], task_data['author_username'])
         self.assertEqual(response.data['created_at'], task_data['created_at'])
         self.assertEqual(response.data['due_date'], task_data['due_date'])
@@ -32,15 +29,31 @@ class ScrumBoardTest(BaseTestSetup):
         
         # Check database correct created
         self.assertTrue(Task.objects.filter(title='Test').exists())
+        
+    
+    def test_put_scrumboard(self):
+        task = self.create_task()
+        self.assertTrue(Task.objects.filter(id=task.id).exists())
+        
+        # Send PUT request to update the task
+        response = self.client.put(f'/tasks/{task.id}/', data=self.UPDATED_TASK_DATA, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        # Refresh task from database
+        task.refresh_from_db()
+
+        # Check updated data
+        self.assertEqual(task.title, self.UPDATED_TASK_DATA['title'])
+        self.assertEqual(task.description, self.UPDATED_TASK_DATA['description'])
+        self.assertEqual(task.priority, self.UPDATED_TASK_DATA['priority'])
+        self.assertEqual(task.color, self.UPDATED_TASK_DATA['color'])
+        self.assertEqual(task.board_column, self.UPDATED_TASK_DATA['board_column'])
     
     
     def test_delete_scrumboard(self):
-        task_data = dict(self.TEST_TASK_DATA)
-        task_data['author'] = self.user
-        task = Task.objects.create(**task_data)
-        
-        # Check task created
+        task = self.create_task()
         self.assertTrue(Task.objects.filter(id=task.id).exists())
+        
         # Delete the task
         response = self.client.delete(f'/tasks/{task.id}/')
         
